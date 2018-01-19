4fb3c1706ae66511360928978e069cf057f94de1
Luis Rovirosa <luisrovirosa@gmail.com>
2018-01-19 09:10:39 +0100

    print date

---
+++ print-date/src/print_date.js
@@ -0,0 +1,7 @@
+class PrintDate {
+  printCurrentDate() {
+    return console.log(new Date());
+  }
+}
+
+module.exports.PrintDate = PrintDate;
---
+++ print-date/test/print_date_test.js
@@ -0,0 +1,12 @@
+let assert = require('assert');
+let PrintDate = require('../src/print_date').PrintDate;
+
+describe('PrintDate', function() {
+  it('change_this_name', function() {
+    let printDate = new PrintDate();
+
+    printDate.printCurrentDate();
+
+    // I don't know how to test it
+  });
+});
