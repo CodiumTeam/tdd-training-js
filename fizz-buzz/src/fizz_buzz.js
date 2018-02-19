a48265e7c8aac07729bb091f67e967d255d1737b
Luis Rovirosa <luisrovirosa@gmail.com>
2018-02-19 08:30:00 +0100

    adds fizz buzz

---
+++ fizz-buzz/src/fizz_buzz.js
@@ -0,0 +1,5 @@
+class FizzBuzz {
+
+}
+
+module.exports.FizzBuzz = FizzBuzz;
---
+++ fizz-buzz/test/fizz_buzz_test.js
@@ -0,0 +1,9 @@
+let assert = require('assert');
+let FizzBuzz = require('../src/fizz_buzz').FizzBuzz;
+
+describe('FizzBuzz', function() {
+  it('change_this_name', function() {
+    let fizzBuzz = new FizzBuzz();
+    assert.equal(true, true);
+  });
+});
