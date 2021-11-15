d3ec75ba3adb36d445f2a761fd4afb4320c15f68
Luis Rovirosa Mairlot <luis@codium.team>
2021-11-15 11:09:31 +0100

    [refactor] reduce test duplicity

--- fizz-buzz/test/fizz_buzz.test.js
+++ fizz-buzz/test/fizz_buzz.test.js
@@ -9,25 +9,13 @@ describe('FizzBuzz', () => {
     expect(values.length).toBe(100);
   });
 
-  it('returns the number 1', () => {
+  it('returns the number when is not multiple of 3, 5 nor 15', () => {
     let fizzBuzz = new FizzBuzz();
 
     let values = fizzBuzz.getValues();
 
     expect(values[0]).toBe(1);
-  });
-  it('returns the number 2', () => {
-    let fizzBuzz = new FizzBuzz();
-
-    let values = fizzBuzz.getValues();
-
     expect(values[1]).toBe(2);
-  });
-  it('returns the number 4', () => {
-    let fizzBuzz = new FizzBuzz();
-
-    let values = fizzBuzz.getValues();
-
     expect(values[3]).toBe(4);
   });
 });
