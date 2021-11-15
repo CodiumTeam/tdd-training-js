9f6fdd39e24b7a978be291c9a0cd7bb95a7559b9
Luis Rovirosa Mairlot <luis@codium.team>
2021-11-15 11:07:04 +0100

    [refactor] simplify

--- fizz-buzz/src/fizz_buzz.js
+++ fizz-buzz/src/fizz_buzz.js
@@ -3,15 +3,7 @@ class FizzBuzz {
     getValues() {
         let values = [];
         for (let i = 0; i < 100; i++) {
-            if (i === 0) {
-                values.push(1);
-            }else if (i === 1){
-                values.push(2);
-            } else if (i === 3){
-                values.push(4);
-            } else {
-                values.push(null);
-            }
+            values.push(i + 1);
         }
 
         return values;
