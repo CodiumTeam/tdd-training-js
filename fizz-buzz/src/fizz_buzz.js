dbc255d7bf991780b08f1fc8cc1ccb4c3b936821
Luis Rovirosa Mairlot <luis@codium.team>
2021-11-15 11:04:36 +0100

    [refactor] reduce complexity

--- fizz-buzz/src/fizz_buzz.js
+++ fizz-buzz/src/fizz_buzz.js
@@ -5,7 +5,7 @@ class FizzBuzz {
         for (let i = 0; i < 100; i++) {
             if (i === 3){
                 values.push(4);
-            }else if (i !== 1) {
+            }else if (i === 0) {
                 values.push(1);
             } else {
                 values.push(2);
