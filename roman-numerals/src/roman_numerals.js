7dc332c36d19d7dcebc90d60745da7ee0c6ae7e5
Luis Rovirosa Mairlot <luis@codium.team>
2021-11-16 13:03:14 +0100

    simplify

--- roman-numerals/src/roman_numerals.js
+++ roman-numerals/src/roman_numerals.js
@@ -3,13 +3,12 @@ class RomanNumerals {
         let translation = {
           1: 'I',
           5: 'V',
-          10: 'X',
         };
-        let values = Object.keys(translation);
-        if (decimal >= values[0] && decimal < values[1]) {
-            return translation[1] + this.toRoman(decimal - 1);
-        } else if (decimal >= values[1] && decimal < values[2]){
-            return translation[5] + this.toRoman(decimal - 5);
+
+        for (const value of Object.keys(translation).reverse()) {
+            if (decimal >= value){
+                return translation[value] + this.toRoman(decimal - value);
+            }
         }
         return '';
     }
