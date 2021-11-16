ce6d60bd04c50b5e38e081b42fa35c5653e31317
Luis Rovirosa Mairlot <luis@codium.team>
2021-11-16 10:59:28 +0100

    translate 5 to roman

--- roman-numerals/src/roman_numerals.js
+++ roman-numerals/src/roman_numerals.js
@@ -2,6 +2,8 @@ class RomanNumerals {
     toRoman(decimal) {
         if (decimal > 0 && decimal <= 3) {
             return 'I' + this.toRoman(decimal - 1);
+        } else if (decimal === 5){
+            return 'V';
         }
         return '';
     }
