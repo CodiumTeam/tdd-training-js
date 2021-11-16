2e9668af62f146a330717428511fb3e3979fca12
Luis Rovirosa Mairlot <luis@codium.team>
2021-11-16 10:56:39 +0100

    increase duplicity to be able to refactor

--- roman-numerals/src/roman_numerals.js
+++ roman-numerals/src/roman_numerals.js
@@ -1,11 +1,13 @@
 class RomanNumerals {
   toRoman(decimal) {
-    if (decimal === 1) {
-      return 'I';
+    if (decimal === 0) {
+      return '';
+    } else if (decimal === 1) {
+      return 'I' + this.toRoman(0);
     } else if (decimal ===2){
-      return 'II';
+      return 'I' + this.toRoman(1);
     } else if (decimal === 3) {
-      return 'III';
+      return 'I' + this.toRoman(2);
     }
   }
 }
