19e841ac30002ed2b3a4f3ddfcafc2a756700955
Luis Rovirosa Mairlot <luis@codium.team>
2021-11-16 10:58:36 +0100

    change the if

--- roman-numerals/src/roman_numerals.js
+++ roman-numerals/src/roman_numerals.js
@@ -1,11 +1,10 @@
 class RomanNumerals {
-  toRoman(decimal) {
-    if (decimal === 0) {
-      return '';
-    } else if (decimal <= 3) {
-      return 'I' + this.toRoman(decimal - 1);
+    toRoman(decimal) {
+        if (decimal > 0 && decimal <= 3) {
+            return 'I' + this.toRoman(decimal - 1);
+        }
+        return '';
     }
-  }
 }
 
 module.exports = RomanNumerals;
