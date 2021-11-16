e7ecd0cd8157ba8bcfd43a427920837ecc6bc64a
Luis Rovirosa Mairlot <luis@codium.team>
2021-11-16 11:03:17 +0100

    improve symmetry

--- roman-numerals/src/roman_numerals.js
+++ roman-numerals/src/roman_numerals.js
@@ -1,9 +1,13 @@
 class RomanNumerals {
     toRoman(decimal) {
-        if (decimal > 0 && decimal <= 3) {
-            return 'I' + this.toRoman(decimal - 1);
-        } else if (decimal >= 5){
-            return 'V' + this.toRoman(decimal-5);
+        let translation = {
+          1: 'I',
+          5: 'V',
+        };
+        if (decimal >= 1 && decimal < 5) {
+            return translation[1] + this.toRoman(decimal - 1);
+        } else if (decimal >= 5 && decimal < 10){
+            return translation[5] + this.toRoman(decimal - 5);
         }
         return '';
     }
