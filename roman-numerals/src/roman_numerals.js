f1cd6e59c56ba4ef214b5a01f013be0b5cd2e963
Luis Rovirosa Mairlot <luis@codium.team>
2021-11-16 10:57:47 +0100

    reduce duplicity

--- roman-numerals/src/roman_numerals.js
+++ roman-numerals/src/roman_numerals.js
@@ -2,11 +2,7 @@ class RomanNumerals {
   toRoman(decimal) {
     if (decimal === 0) {
       return '';
-    } else if (decimal === 1) {
-      return 'I' + this.toRoman(decimal - 1);
-    } else if (decimal ===2){
-      return 'I' + this.toRoman(decimal - 1);
-    } else if (decimal === 3) {
+    } else if (decimal <= 3) {
       return 'I' + this.toRoman(decimal - 1);
     }
   }
