9332a5a0533c339079a59877078b313b2e6648ac
Jose Cabrera <jose@codium.team>
2021-11-17 18:18:56 +0100

    Add styles

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import {useState} from "react";
+import { useState } from 'react';
 
 function App() {
   function handleChange(event) {
-    if (event.key === "Enter") {
+    if (event.key === 'Enter') {
       const newItem = event.target.value;
       setItems([...items, newItem]);
     }
@@ -17,12 +15,16 @@ function App() {
       <header className="App-header">
         <h1>TODO App</h1>
       </header>
-      <input type="text" placeholder="What needs to be done?" onKeyPress={handleChange}/>
+      <input
+        type="text"
+        placeholder="What needs to be done?"
+        onKeyPress={handleChange}
+      />
       <ul className="todo-list">
         <li>Learn TDD</li>
         <li>Start writing a test</li>
-        { items.map((item, index) => {
-          return <li key={index}>{item}</li>
+        {items.map((item, index) => {
+          return <li key={index}>{item}</li>;
         })}
       </ul>
     </div>
--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className="root">
+      <img
+        src="http://www.codium.team/img/resources/codium-icon-black.svg"
+        alt="Codium Team"
+        title="Codium team"
+      />
+      <App />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
