204ef163c32418e4fe1553d15c411f37b10969ce
Luis Rovirosa Mairlot <luis@codium.team>
2021-11-17 15:03:33 +0100

    [user registration] refactor extract beforeEach

--- user-registration/test/user_registration.test.js
+++ user-registration/test/user_registration.test.js
@@ -4,15 +4,24 @@ const UserDatabase = require('../src/user_database');
 const User = require('../src/user');
 
 describe('UserRegistration', () => {
+
+  let userIdGenerator;
+  let userDatabase;
+  let userRegistration;
+
+  beforeEach(() => {
+    userIdGenerator = new UserIdGenerator();
+    userDatabase = new UserDatabase();
+    userRegistration = new UserRegistration(userDatabase, userIdGenerator);
+  });
+
   it('persist the user with the id', () => {
-    const userIdGenerator = new UserIdGenerator();
     userIdGenerator.generateId = jest.fn().mockReturnValue('anId');
-    const userDatabase = new UserDatabase();
     jest.spyOn(userDatabase, 'save').mockName('save');
-    const userRegistration = new UserRegistration(userDatabase, userIdGenerator);
 
     userRegistration.execute('any@email.com', 'validPassword');
 
-    expect(userDatabase.save).toHaveBeenCalledWith(new User('anId', 'any@email.com', 'validPassword'));
+    let expectedUser = new User('anId', 'any@email.com', 'validPassword');
+    expect(userDatabase.save).toHaveBeenCalledWith(expectedUser);
   });
 });
