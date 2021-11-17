2ead40b7c93caac6fbe08e1b5e223e19de508bf2
Luis Rovirosa Mairlot <luis@codium.team>
2021-11-17 15:20:23 +0100

    [user registration] reformat

--- user-registration/src/user_registration.js
+++ user-registration/src/user_registration.js
@@ -1,28 +1,29 @@
 const User = require('./user');
 
 class UserRegistration {
-  userDatabase;
-  userIdGenerator;
-  constructor(userDatabase, userIdGenerator) {
-    this.userDatabase = userDatabase;
-    this.userIdGenerator = userIdGenerator;
-  }
+    userDatabase;
+    userIdGenerator;
 
-  execute(email, password) {
-    if (this.invalidPassword(password)) {
-      return;
+    constructor(userDatabase, userIdGenerator) {
+        this.userDatabase = userDatabase;
+        this.userIdGenerator = userIdGenerator;
     }
-    if (this.userDatabase.findByEmail(email)) {
-      return;
-    }
-    let id = this.userIdGenerator.generateId();
-    this.userDatabase.save(new User(id, email, password));
-  }
 
-  invalidPassword(password) {
-    return password.length <= 8
-        || !password.includes('_');
-  }
+    execute(email, password) {
+        if (this.invalidPassword(password)) {
+            return;
+        }
+        if (this.userDatabase.findByEmail(email)) {
+            return;
+        }
+        let id = this.userIdGenerator.generateId();
+        this.userDatabase.save(new User(id, email, password));
+    }
+
+    invalidPassword(password) {
+        return password.length <= 8
+            || !password.includes('_');
+    }
 }
 
 module.exports = UserRegistration;
