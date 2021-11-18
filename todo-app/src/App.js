f6bbcaad8b207612f600e3b0a01d5e55839eeb0f
Jordi Anguela <jordi@codium.team>
2021-11-18 08:53:02 +0100

    [todo-app] prepare files for development

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,65 +1,12 @@
 import { useState } from 'react';
 
 function App() {
-  function handleOnKeyPress(event) {
-    if (event.key === 'Enter') {
-      const newItem = {
-        title: event.target.value,
-        checked: false
-      };
-      setItems([...items, newItem]);
-      setInputText('');
-    }
-  }
-
-  function handleOnChange(event) {
-    setInputText(event.target.value);
-  }
-
-  function handleOnCheckboxChange(index, checkboxState) {
-    const item = items[index];
-    const updatedItem = {...item, checked: checkboxState}
-    const newListItems = [...items];
-    newListItems[index] = updatedItem;
-    setItems(newListItems)
-  }
-
-  const firstDefaultItem = {
-    title: 'Learn TDD',
-    checked: false
-  }
-  const secondDefaultItem = {
-    title: 'Start writing a test',
-    checked: false
-  }
-  const defaultItems = [firstDefaultItem, secondDefaultItem];
-  const [items, setItems] = useState(defaultItems);
-  const [inputText, setInputText] = useState("");
-
   return (
     <div className="App">
       <header className="App-header">
-        <h1>TODO App</h1>
+        title
       </header>
-      <input
-        type="text"
-        placeholder="What needs to be done?"
-        onKeyPress={handleOnKeyPress}
-        value={inputText}
-        onChange={handleOnChange}
-      />
-      <ul className="todo-list">
-        {items.map((item, index) => {
-          return (
-            <li key={index} className={item.checked ? 'completed' : ''}>
-              <input type="checkbox" onChange={(event) => {
-                handleOnCheckboxChange(index, event.target.checked);
-              }}/>
-              <p>{item.title}</p>
-            </li>
-          );
-        })}
-      </ul>
+      todo-list here
     </div>
   );
 }
--- todo-app/src/App.test.js
+++ todo-app/src/App.test.js
@@ -2,66 +2,8 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
 import App from './App';
 
-test('displays TODO App title', () => {
+test('xxx', () => {
   render(<App />);
 
-  const titleElement = screen.getByText(/TODO App/i);
-  expect(titleElement).toBeInTheDocument();
-});
-
-test('displays input to create a new todo', () => {
-  render(<App />);
-
-  const createNewElement = screen.getByRole('textbox');
-  expect(createNewElement).toBeInTheDocument();
-  expect(createNewElement.placeholder).toBe('What needs to be done?');
-});
-
-test('displays a list with 2 items by default', () => {
-  render(<App />);
-
-  const items = screen.getAllByRole('listitem');
-  expect(items).toHaveLength(2);
-  expect(items[0]).toHaveTextContent("Learn TDD");
-  expect(items[1]).toHaveTextContent("Start writing a test");
-});
-
-test('add new todo into the list when pressing <enter>', () => {
-  render(<App />);
-
-  userEvent.type(screen.getByRole('textbox'), 'my new todo{enter}')
-
-  const items = screen.getAllByRole('listitem');
-  expect(items).toHaveLength(3);
-  expect(items[2]).toHaveTextContent("my new todo");
-});
-
-test('once the element has been added to the list, input text should be empty.', () => {
-  render(<App />);
-
-  userEvent.type(screen.getByRole('textbox'), 'my new todo{enter}')
-
-  const input = screen.getByRole('textbox');
-  expect(input.value).toBe("");
-});
-
-test('add css style class when completing a TODO item', () => {
-  render(<App />);
-
-  userEvent.click(screen.getAllByRole('checkbox')[0]);
-
-  const todoItems = screen.getAllByRole('listitem');
-  const firstItem = todoItems[0];
-  expect(firstItem).toHaveClass("completed");
-});
-
-test('removes css style class when toggling a completed TODO item', () => {
-  render(<App />);
-
-  userEvent.click(screen.getAllByRole('checkbox')[0]);
-  userEvent.click(screen.getAllByRole('checkbox')[0]);
-
-  const todoItems = screen.getAllByRole('listitem');
-  const firstItem = todoItems[0];
-  expect(firstItem).not.toHaveClass("completed");
+  expect(true).toBe(true);
 });
