83c3f28b2ad9869fc1984d4a55461b410db01920
Jose Cabrera <jose@codium.team>
2021-11-19 10:36:28 +0100

    remove references to App

--- coffee-machine-react/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event'
-import App from './App';
-
-test('xxx', () => {
-  render(<App />);
-
-  expect(true).toBe(true);
-});
