12d7f1905d5f57e02bf60777489fe1e2c9522302
Jose Cabrera <jose@codium.team>
2021-11-19 10:41:29 +0100

    Validate that three buttons is Tea, Coffee and Chocolate

--- coffee-machine-react/src/DrinkMaker.test.js
+++ coffee-machine-react/src/DrinkMaker.test.js
@@ -8,4 +8,7 @@ test('Should render availables drinks', () => {
   const drinkButton = within(list).getAllByRole('button');
 
   expect(drinkButton).toHaveLength(3);
+  expect(drinkButton[0]).toHaveTextContent('Tea');
+  expect(drinkButton[1]).toHaveTextContent('Coffee');
+  expect(drinkButton[2]).toHaveTextContent('Chocolate');
 });
