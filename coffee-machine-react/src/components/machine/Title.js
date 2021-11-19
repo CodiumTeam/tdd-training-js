9103276915b799e5a5a39ee7cdd8d1eb05c38e3c
Jose Cabrera <jose@codium.team>
2021-11-19 22:14:45 +0100

    Change some styles with message

---
+++ coffee-machine-react/src/components/machine/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './title.css';
+
+function Title() {
+  return (
+    <div className="title">
+      <p>COFFEE TO GO</p>
+    </div>
+  );
+}
+
+export default Title;
