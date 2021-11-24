5d4da84f9d5302db992bd078b1b179a81d7d162f
Jose Cabrera <jose@codium.team>
2021-11-24 11:53:41 +0100

    [Coffe-machine]: add image for every king of drink

--- coffee-machine-react/src/components/DrinkMaker/DrinkMakerOutput.js
+++ coffee-machine-react/src/components/DrinkMaker/DrinkMakerOutput.js
@@ -1,41 +1,59 @@
 import React from 'react';
 import { drinkMaker } from '../../drinkMaker';
 
-import './drink-maker.css';
+import './drink-maker-output.css';
 
 const DRINKS_IMAGES = {
   T: '/images/tea.png',
-  O: '/images/orange.png',
+  'T-1-0': '/images/tea-with-sugar-stick.png',
+  'T-2-0': '/images/tea-with-two-sugar-stick.png',
   C: '/images/coffee.png',
+  'C-1-0': '/images/coffee-with-sugar-stick.png',
+  'C-2-0': '/images/coffee-with-two-sugar-stick.png',
   H: '/images/chocolate.png',
+  'H-1-0': '/images/chocolate-with-sugar-stick.png',
+  'H-2-0': '/images/chocolate-with-two-sugar-stick.png',
+  O: '/images/orange.png',
+  'O-1-0': '/images/orange-with-sugar-stick.png',
+  'O-2-0': '/images/orange-with-two-sugar-stick.png',
 };
 
 const AVAILABLE_COMMANDS = ['T', 'C', 'H', 'O', 'M'];
+const getImagePathFromCommand = (primaryCommand, restOfCommand) => {
+  const cleanedRestOfCommand = restOfCommand.filter(Boolean);
+  const hasMoreCommand = cleanedRestOfCommand.length > 0;
+  const imageKey =
+    primaryCommand +
+    (hasMoreCommand ? `-${cleanedRestOfCommand.join('-')}` : '');
+  const imgSrc = DRINKS_IMAGES[imageKey];
+
+  return imgSrc;
+};
 
 function DrinkMakerOutput({ command }) {
   if (!command) return null;
 
-  const [executedCommand, ...rest] = command.split(':');
+  const [primaryCommand, ...restOfCommand] = command.split(':');
 
-  if (!executedCommand) return null;
+  if (!primaryCommand) return null;
 
-  if (!AVAILABLE_COMMANDS.includes(executedCommand)) {
-    throw new Error(`Command "${executedCommand}" is not a valid command`);
+  if (!AVAILABLE_COMMANDS.includes(primaryCommand)) {
+    throw new Error(`Command "${primaryCommand}" is not a valid command`);
   }
 
-  const isSendingAMessage = executedCommand === 'M';
-  const imgSrc = DRINKS_IMAGES[executedCommand];
-  const message = rest.join('');
+  const isSendingAMessage = primaryCommand === 'M';
+  const imgSrc = getImagePathFromCommand(primaryCommand, restOfCommand);
+  const imageClassName = command.replace(/:/g, '-');
 
   drinkMaker.execute(command);
 
   return (
     <div
       role="alert"
-      className={`drink-maker-image drink-maker-image--${executedCommand}`}
+      className={`drink-maker-image drink-maker-image--${primaryCommand} ${imageClassName}`}
     >
       {isSendingAMessage ? (
-        <MessageBox text={message} />
+        <MessageBox text={restOfCommand.join('')} />
       ) : (
         <img src={imgSrc} alt="Selected drink" />
       )}
