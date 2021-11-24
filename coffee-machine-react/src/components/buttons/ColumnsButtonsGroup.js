ef5a001da0a8f712faca59d0b836c721293d36a2
Jose Cabrera <jose@codium.team>
2021-11-24 15:00:12 +0100

    [coffee-machine]: Styled clicked button

--- coffee-machine-react/src/components/buttons/ColumnsButtonsGroup.js
+++ coffee-machine-react/src/components/buttons/ColumnsButtonsGroup.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './columns-buttons-group.css';
 
 function ButtonsGroup({ children }) {
   const childrens = React.Children.toArray(children);
+  const [selectedButton, setSelectedButton] = useState('');
+
+  // This is just for "hiding complexity" to developers who are learning TDD
+  // It's not a good practice to use it in a production code
+  const handleClick = (event) => {
+    const target = event.target;
+    const classNames = target.className || '';
+
+    if (classNames.includes('button')) {
+      const text = target.textContent;
+      setSelectedButton(text);
+    }
+  };
 
   return (
     <ul className="button-group">
       {childrens.map((child, index) => {
-        return <li key={index}>{child}</li>;
+        const childText = child.props.text;
+        return (
+          <li
+            key={index}
+            onClick={handleClick}
+            className={childText === selectedButton ? 'is-selected' : ''}
+          >
+            {child}
+          </li>
+        );
       })}
     </ul>
   );
