f6611842ce03d3fed1fd307e09520cbeb0be2b08
Jose Cabrera <jose@codium.team>
2022-02-21 15:30:07 +0100

    Remove onClick required prop

--- coffee-machine-react/src/ui/buttons/Button.js
+++ coffee-machine-react/src/ui/buttons/Button.js
@@ -2,12 +2,10 @@ import React from 'react';
 
 import './button.css';
 
-function Button({ text, onClick, small }) {
+function Button({ text, onClick = () => {}, small = false }) {
   return (
     <button
-      onClick={() => {
-        onClick(text);
-      }}
+      onClick={onClick}
       className={`button ${small ? 'small' : ''}`}
     >
       {text}
