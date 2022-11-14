eb966f63b7cd91deb5e2dad971b36ed73310cdf8
Jordi Anguela <jordi@codium.team>
2022-11-14 17:01:35 +0100

    a solution for FizzBuzz (first part)

--- fizz-buzz/src/fizz_buzz.js
+++ fizz-buzz/src/fizz_buzz.js
@@ -1,5 +1,33 @@
 class FizzBuzz {
 
+  generateList() {
+    const list = [];
+
+    for (let i = 0; i < 100; i++) {
+      if (this._isFizzBuzz(i)) {
+        list.push('FizzBuzz');
+      } else if (this._isFizz(i)) {
+        list.push('Fizz');
+      } else if (this._isBuzz(i)) {
+        list.push('Buzz');
+      } else {
+        list.push("" + (i + 1));
+      }
+    }
+    return list;
+  }
+
+  _isFizzBuzz(i) {
+    return this._isFizz(i) && this._isBuzz(i);
+  }
+
+  _isFizz(i) {
+    return (i + 1) % 3 === 0;
+  }
+
+  _isBuzz(i) {
+    return (i + 1) % 5 === 0;
+  }
 }
 
 module.exports = FizzBuzz;
--- fizz-buzz/test/fizz_buzz.test.js
+++ fizz-buzz/test/fizz_buzz.test.js
@@ -1,10 +1,57 @@
 const FizzBuzz = require('../src/fizz_buzz');
+const expect = require("expect");
 
 describe('FizzBuzz', () => {
-  it('change_this_name', () => {
+  it('should return 100 elements', () => {
+    const fizzBuzz = new FizzBuzz();
 
-    new FizzBuzz();
+    const list = fizzBuzz.generateList();
 
-    expect(true).toBe(true);
+    expect(list.length).toBe(100);
   });
+
+  it('should return numbers when other rules do not apply', () => {
+    const fizzBuzz = new FizzBuzz();
+
+    const list = fizzBuzz.generateList();
+
+    validatePosition(list, 1, '1');
+    validatePosition(list, 2, '2');
+    validatePosition(list, 4, '4');
+  });
+
+  it('should return Fizz when multiple of 3', () => {
+    const fizzBuzz = new FizzBuzz();
+
+    const list = fizzBuzz.generateList();
+
+    validatePosition(list, 3, 'Fizz');
+    validatePosition(list, 6, 'Fizz');
+    validatePosition(list, 9, 'Fizz');
+  });
+
+  it('should return Buzz when multiple of 5', () => {
+    const fizzBuzz = new FizzBuzz();
+
+    const list = fizzBuzz.generateList();
+
+    validatePosition(list, 5, 'Buzz');
+    validatePosition(list, 10, 'Buzz');
+    validatePosition(list, 20, 'Buzz');
+  });
+
+  it('should return FizzBuzz when multiple of 15', () => {
+    const fizzBuzz = new FizzBuzz();
+
+    const list = fizzBuzz.generateList();
+
+    validatePosition(list, 15, 'FizzBuzz');
+    validatePosition(list, 30, 'FizzBuzz');
+    validatePosition(list, 45, 'FizzBuzz');
+  });
+
+  function validatePosition(list, position, expected) {
+    expect(list[position - 1]).toBe(expected);
+  }
 });
+
