d8d61779ea21f597287bf44007ac51a91fbf6b71
Jordi Anguela <jordi@codium.team>
2022-11-14 17:24:16 +0100

    CoffeeMachine: finish 1st iteration basic tests

--- coffee-machine/src/coffee_machine.js
+++ coffee-machine/src/coffee_machine.js
@@ -1,11 +1,38 @@
 class CoffeeMachine {
+  sugarLevel = 0;
+
   constructor(drinkMaker) {
     this.drinkMaker = drinkMaker;
   }
 
-  tea() {
-    this.drinkMaker.execute('T::');
+  selectOneSugar() {
+    this.sugarLevel = 1;
   }
+
+  selectTwoSugar() {
+    this.sugarLevel = 2;
+  }
+
+  selectCoffee() {
+    this._prepareDrink('C');
+  }
+
+  selectTea() {
+    this._prepareDrink('T');
+  }
+
+  selectHotChocolate() {
+    this._prepareDrink('H');
+  }
+
+  _prepareDrink(product) {
+    if (this.sugarLevel === 0) {
+      this.drinkMaker.execute(product + '::');
+    } else {
+      this.drinkMaker.execute(product + ':' + this.sugarLevel + ':0');
+    }
+  }
+
 }
 
 module.exports = CoffeeMachine;
--- coffee-machine/test/coffee_machine.test.js
+++ coffee-machine/test/coffee_machine.test.js
@@ -2,13 +2,55 @@ let CoffeeMachine = require('../src/coffee_machine');
 let DrinkMaker = require('../src/drink_maker');
 
 describe('CoffeeMachine', function () {
+  it('User is able to select "Coffee" without sugar', () => {
+    const maker = new DrinkMaker();
+    jest.spyOn(maker, 'execute');
+    const coffeeMachine = new CoffeeMachine(maker);
+
+    coffeeMachine.selectCoffee();
+
+    expect(maker.execute).toHaveBeenCalledWith('C::');
+  });
+
   it('User is able to select "Tea" without sugar', () => {
     const maker = new DrinkMaker();
     jest.spyOn(maker, 'execute');
     const coffeeMachine = new CoffeeMachine(maker);
 
-    coffeeMachine.tea();
+    coffeeMachine.selectTea();
 
     expect(maker.execute).toHaveBeenCalledWith('T::');
+  });
+
+  it('User is able to select "Hot chocolate" without sugar', () => {
+    const maker = new DrinkMaker();
+    jest.spyOn(maker, 'execute');
+    const coffeeMachine = new CoffeeMachine(maker);
+
+    coffeeMachine.selectHotChocolate();
+
+    expect(maker.execute).toHaveBeenCalledWith('H::');
+  });
+
+  it('User is able select a product with one sugar', () => {
+    const maker = new DrinkMaker();
+    jest.spyOn(maker, 'execute');
+    const coffeeMachine = new CoffeeMachine(maker);
+
+    coffeeMachine.selectOneSugar();
+    coffeeMachine.selectCoffee();
+
+    expect(maker.execute).toHaveBeenCalledWith('C:1:0');
+  });
+
+  it('User is able select a product with two sugar', () => {
+    const maker = new DrinkMaker();
+    jest.spyOn(maker, 'execute');
+    const coffeeMachine = new CoffeeMachine(maker);
+
+    coffeeMachine.selectTwoSugar();
+    coffeeMachine.selectCoffee();
+
+    expect(maker.execute).toHaveBeenCalledWith('C:2:0');
   });
 });
