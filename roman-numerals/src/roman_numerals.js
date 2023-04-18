f33a7b87d7108aeaa8f8a9c46afa1121663fec8d
Jose Cabrera <jose@codium.team>
2023-04-18 11:17:29 +0200

    refactor

--- roman-numerals/src/roman_numerals.js
+++ roman-numerals/src/roman_numerals.js
@@ -1,31 +1,29 @@
 class RomanNumerals {
     convert(number) {
+        const numbers = {
+            1: 'I',
+            5: 'V'
+        }
         if (number === 1) {
-            return 'I';
+            return numbers[1] + this.convert(number - 1);
         }
-
         if (number === 2) {
-            return 'II';
+            return numbers[1] + this.convert(number - 1);
         }
-
         if (number === 3) {
-            return 'III';
+            return numbers[1] + this.convert(number - 1);
         }
-
         if (number === 5) {
-            return 'V';
+            return numbers[5] + this.convert(number - 5);
         }
-
         if (number === 6) {
-            return 'VI';
+            return numbers[5] + this.convert(number - 5);
         }
-
         if (number === 7) {
-            return 'VII';
+            return numbers[5] + this.convert(number - 5);
         }
-
         if (number === 8) {
-            return 'VIII';
+            return numbers[5] + this.convert(number - 5);
         }
 
         return '';
