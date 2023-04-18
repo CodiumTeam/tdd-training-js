722ffbe9b4c7eea55cebad5a43abf418262330b6
Jose Cabrera <jose@codium.team>
2023-04-18 11:12:58 +0200

    convert to romains

--- roman-numerals/src/roman_numerals.js
+++ roman-numerals/src/roman_numerals.js
@@ -1,7 +1,35 @@
 class RomanNumerals {
-  changeThisName() {
-    return true;
-  }
+    convert(number) {
+        if (number === 1) {
+            return 'I';
+        }
+
+        if (number === 2) {
+            return 'II';
+        }
+
+        if (number === 3) {
+            return 'III';
+        }
+
+        if (number === 5) {
+            return 'V';
+        }
+
+        if (number === 6) {
+            return 'VI';
+        }
+
+        if (number === 7) {
+            return 'VII';
+        }
+
+        if (number === 8) {
+            return 'VIII';
+        }
+
+        return '';
+    }
 }
 
 module.exports = RomanNumerals;
--- roman-numerals/test/roman_numerals.test.js
+++ roman-numerals/test/roman_numerals.test.js
@@ -1,9 +1,20 @@
 let RomanNumerals = require('../src/roman_numerals');
 
 describe('Roman Numerals', () => {
-  it('change_this_name', () => {
-    let romanNumerals = new RomanNumerals();
+     test.each([
+         [1, 'I'],
+         [2, 'II'],
+         [3, 'III'],
+         [5, 'V'],
+         [6, 'VI'],
+         [7, 'VII'],
+         [8, 'VIII'],
+     ])(
+        'For %i returns %s',
+        (input, expected) => {
+            let romanNumerals = new RomanNumerals();
 
-    expect(romanNumerals.changeThisName()).toBe(true);
-  });
+            expect(romanNumerals.convert(input)).toBe(expected);
+        }
+    );
 });
