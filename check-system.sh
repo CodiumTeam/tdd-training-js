8ac5d0f9c6241ecf593ac0616a62d467ad67a6a7
Jose Cabrera <jose@codium.team>
2023-08-14 14:30:20 +0200

    Update check-system.sh
    Remove interative -it

--- check-system.sh
+++ check-system.sh
@@ -39,7 +39,7 @@ function validateDocker() {
     fi
 
     echo -n "Validating docker mount permissions..."
-    (docker run -it --rm -v ${PWD}:/kata codiumteam/tdd-training-js ls) > /dev/null
+    (docker run --rm -v ${PWD}:/kata codiumteam/tdd-training-js ls) > /dev/null
     if [ $? -ne 0 ]; then
       echo "Error"
       echo "Are you sure that you have permissions to mount your volumes?"
