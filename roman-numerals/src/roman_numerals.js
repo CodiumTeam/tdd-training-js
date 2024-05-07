44a1c6367a7401730a76f53ca2e12db57d74d45d
Luis Rovirosa Mairlot <luis@codium.team>
2024-05-07 09:32:50 +0200

    [roman-numerals] Rollback

--- roman-numerals/src/roman_numerals.js
+++ roman-numerals/src/roman_numerals.js
@@ -1,33 +1,7 @@
 class RomanNumerals {
-    convert(number) {
-        const numbers = {
-            1: 'I',
-            5: 'V'
-        }
-        if (number === 1) {
-            return numbers[1] + this.convert(number - 1);
-        }
-        if (number === 2) {
-            return numbers[1] + this.convert(number - 1);
-        }
-        if (number === 3) {
-            return numbers[1] + this.convert(number - 1);
-        }
-        if (number === 5) {
-            return numbers[5] + this.convert(number - 5);
-        }
-        if (number === 6) {
-            return numbers[5] + this.convert(number - 5);
-        }
-        if (number === 7) {
-            return numbers[5] + this.convert(number - 5);
-        }
-        if (number === 8) {
-            return numbers[5] + this.convert(number - 5);
-        }
-
-        return '';
-    }
+  changeThisName() {
+    return true;
+  }
 }
 
 module.exports = RomanNumerals;
--- roman-numerals/test/roman_numerals.test.js
+++ roman-numerals/test/roman_numerals.test.js
@@ -1,20 +1,9 @@
 let RomanNumerals = require('../src/roman_numerals');
 
 describe('Roman Numerals', () => {
-     test.each([
-         [1, 'I'],
-         [2, 'II'],
-         [3, 'III'],
-         [5, 'V'],
-         [6, 'VI'],
-         [7, 'VII'],
-         [8, 'VIII'],
-     ])(
-        'For %i returns %s',
-        (input, expected) => {
-            let romanNumerals = new RomanNumerals();
+  it('change_this_name', () => {
+    let romanNumerals = new RomanNumerals();
 
-            expect(romanNumerals.convert(input)).toBe(expected);
-        }
-    );
+    expect(romanNumerals.changeThisName()).toBe(true);
+  });
 });
