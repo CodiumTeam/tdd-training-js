123a9f39b3948c6449ce057b14ea780685150941
Luis Rovirosa Mairlot <luis@codium.team>
2024-05-09 11:07:32 +0200

    [coffee-machine] prepare coffee

--- coffee-machine/src/coffee_machine.js
+++ coffee-machine/src/coffee_machine.js
@@ -1,5 +1,12 @@
 class CoffeeMachine {
+    constructor(drinkMaker) {
+        this._drinkMaker = drinkMaker;
+    }
 
+
+    prepareCoffee() {
+        this._drinkMaker.execute("C::");
+    }
 }
 
 module.exports = CoffeeMachine;
--- coffee-machine/test/coffee_machine.test.js
+++ coffee-machine/test/coffee_machine.test.js
@@ -2,9 +2,13 @@ let CoffeeMachine = require('../src/coffee_machine');
 let DrinkMaker = require('../src/drink_maker');
 
 describe('CoffeeMachine', function () {
-  it('change_this_name', function () {
-    let coffeeMachine = new CoffeeMachine();
+  it('prepare coffee without sugar', function () {
+    const drinkMaker = new DrinkMaker();
+    drinkMaker.execute = jest.fn()
+    let coffeeMachine = new CoffeeMachine(drinkMaker);
 
-    expect(true).toBe(true);
+    coffeeMachine.prepareCoffee();
+
+    expect(drinkMaker.execute).toBeCalledWith("C::");
   });
 });
